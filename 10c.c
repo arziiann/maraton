f1ee5320c80df10d777340b5620594bacf6cc5b7
arziiann <127401577+arziiann@users.noreply.github.com>
2023-03-19 20:56:47 +0400

    Add files via upload

---
+++ 10c.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+void array(int num1, int num2, int* arr)
+{
+	for(int i = num1 + 1; i < num2; i++)
+	{
+		arr[i] = i;
+		printf("%i\n", arr[i]);
+	}
+	printf("%p", arr);
+
+}
+
+
+int main()
+{
+	int num1 = 0;
+	int num2 = 0;
+	
+	puts("Enter one number");	
+	scanf("%i", &num1);
+	
+	puts("Enter two number");
+	scanf("%i", &num2);
+	int* arr = (int*)calloc(num2 - num1, sizeof(int));	
+	array(num1, num2, arr);
+
+}
