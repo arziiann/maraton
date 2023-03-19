2b7a34aebc93c1f318eaf704cbf27c38081cfebe
arziiann <127401577+arziiann@users.noreply.github.com>
2023-03-19 21:23:11 +0400

    Add files via upload

---
+++ 11c.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void zangvac_hakarak_hertakanutyamb(int* array, int size);
+
+
+
+int main()
+{
+	int size = 0;
+	puts("Enter array size");
+	scanf("%i", &size);
+
+	int* array = (int*)calloc(size, sizeof(int));
+	puts("Enter array elements");
+	for(int i = 0; i < size; i++)
+	{
+		scanf( "%i", &array[i]);
+
+	}
+	zangvac_hakarak_hertakanutyamb(array, size);
+
+}
+
+void zangvac_hakarak_hertakanutyamb(int* array, int size)
+{
+	for(int i = size - 1; i >= 0; i--)
+
+	{
+		printf("%i", array[i]);
+
+	}
+
+}
