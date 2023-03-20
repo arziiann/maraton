4442efec2048ed85d429fbf7f15a1f5616437412
arziiann <127401577+arziiann@users.noreply.github.com>
2023-03-20 15:09:10 +0400

    Add files via upload

---
+++ 14c.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int* function(int size);
+
+int main()
+{
+	int number = 0;
+	puts("Enter number");
+	scanf("%i", &number);
+	int* copy = function(number);
+	for(int i = 0; i < number; i++)
+	{
+		printf("%i", copy[i]);
+
+	}
+}
+
+int* function(int size)
+{
+	int* array = (int*)calloc(size, sizeof(int));
+	puts("Enter array number");
+	for(int i = 0; i < size; i++)
+	{
+		scanf("%i", &array[i]);
+		
+	}
+
+	return array;
+}
+
+
+
